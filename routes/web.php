7cbae0a6a6905c71f6df058351239d59debec05d
tonary26 <mpclown5@gmail.com>
2025-09-08 15:27:29 +0300

    Products commit

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Controllers\Products;
+
+
+Route::get('/', function() {
+    return 'Home page';
 });
+
+Route::get('/products', [Products::class, 'index']);
